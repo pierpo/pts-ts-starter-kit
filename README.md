1c330ab5d5f3e6111e10bdb396cede7ff5f731f2
Pierre Poupin <pierrep@theodo.fr>
2018-08-09 22:55:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# pts-ts-starter-kit
+A typescript version of the [pts starter kit](https://github.com/williamngan/pts-starter-kit) 🤓
+
+## Usage
+
+- Clone the repo
+
+```bash
+git clone git@github.com:pierpo/pts-ts-starter-kit
+```
+
+- Install the dependencies
+
+```bash
+yarn
+```
+
+- Start developing
+
+```bash
+yarn start
+```
+
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const AwesomeTypescriptLoader = require('awesome-typescript-loader');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'PTS',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+};
