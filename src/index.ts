3a6ff96be25ffc0361422fa6b039004817fe9c2b
Ben LaVerriere <ben.laverriere@bookbub.com>
2020-06-06 11:51:49 -0400

    upgrade to Pts 0.9.1

--- src/index.ts
+++ src/index.ts
@@ -2,55 +2,53 @@
 // Source code licensed under Apache License 2.0.
 // Copyright © 2017 William Ngan. (https://github.com/williamngan/pts)
 
-import { Group, CanvasSpace, Create, Polygon } from 'pts';
+import {CanvasSpace, Create, Polygon} from 'pts';
 
 // Initiate Space and Form
-const space = new CanvasSpace('#pts').setup({
-  bgcolor: '#09F',
-  resize: true,
-  retina: true,
-});
+const space = new CanvasSpace('#pts').setup({bgcolor: '#09F', resize: true, retina: true});
 const form = space.getForm();
 
-let landmarks: Group;
+let landmarks;
 
 space.add({
+
   start: bound => {
     // Make a face with 30 radial points with slight randomness
     const radius = space.size.minValue().value / 3;
-    landmarks = Create.radialPts(space.center, radius, 30);
-    landmarks.map(p => p.add(50 * (Math.random() - Math.random())));
+    landmarks = Create.radialPts( space.center, radius, 30  );
+    landmarks.map( p => p.add( 50 * (Math.random() - Math.random()) ) );
+
   },
 
   animate: (time, ftime) => {
+
     landmarks[landmarks.length - 1] = space.pointer;
 
     // convex hull the points
-    const hull = Polygon.convexHull(landmarks);
+    const hull = Polygon.convexHull( landmarks );
 
     // eyes' positions
-    const left = space.center.$subtract(50);
-    const right = space.center.$add(50, -50);
-    const leftB = left.clone().toAngle(space.pointer.$subtract(left).angle(), 10, !!left);
-    const rightB = right.clone().toAngle(space.pointer.$subtract(right).angle(), 10, !!right);
+    const left = space.center.$subtract( 50 );
+    const right = space.center.$add( 50, -50 );
+    const leftB = left.clone().toAngle( space.pointer.$subtract( left ).angle(), 10, !!left );
+    const rightB = right.clone().toAngle( space.pointer.$subtract( right ).angle(), 10, !!right );
 
     // draw face and eyes
-    form.fillOnly('rgba(255, 255, 255, 0.5)').polygon(hull);
-    form.fill('#fff').points([left, right], 20, 'circle');
-    form.fill('#123').points([leftB, rightB], 5, 'circle');
+    form.fillOnly('rgba(255, 255, 255, 0.5)').polygon( hull );
+    form.fill('#fff').points( [left, right], 20, 'circle' );
+    form.fill('#123').points( [leftB, rightB], 5, 'circle' );
 
     // draw the hull and pts
-    form.fill('#fff').points(hull, 5, 'circle');
-    form.fill('rgba(0,0,0,.5)').points(landmarks, 2, 'circle');
-    form.fill('#f03').point(space.pointer, 10, 'circle');
+    form.fill('#fff').points( hull, 5, 'circle' );
+    form.fill('rgba(0,0,0,.5)').points( landmarks, 2, 'circle' );
+    form.fill('#f03').point( space.pointer, 10, 'circle' );
 
     // draw mouth
-    form.strokeOnly('#123', 5).line([left.$add(0, 80), right.$add(0, 80)]);
+    form.strokeOnly('#123', 5).line( [left.$add(0, 80), right.$add(0, 80)]);
+
   },
+
 });
 
 // bind mouse events and play animation
-space
-  .bindMouse()
-  .bindTouch()
-  .play();
+space.bindMouse().bindTouch().play();
