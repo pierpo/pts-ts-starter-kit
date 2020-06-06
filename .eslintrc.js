3ca30ebf76c83cf21cb0d909351150bac7aa08c3
Ben LaVerriere <ben.laverriere@bookbub.com>
2020-06-06 12:32:01 -0400

    add prettier so .prettierrc gets used

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   rules: {
     "@typescript-eslint/no-unsafe-assignment": "error",
